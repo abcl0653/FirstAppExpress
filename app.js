22fc07f2c8e88d614029cb889b4fad3d46882e31
Charlie Cai <fd.cailin@outlook.com>
2016-05-10 23:23:54 +0800

    routes

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-var http = require('http');
-var express = require('express');
+var http 			= require('http');
+var express 		= require('express');
+var responseTime 	= require('response-time');
+var errorHandler 	= require('errorhandler');
+var iniParser 		= require('iniParser');
+var logger			= require('morgan');
 
 
 var app = express();
+
+var config = iniParser.parseSync('./config.ini');
+
+var configJson = require('./config.json')[app.get('env')];
+console.log(configJson.db_host);
 
 app.set('view engine', 'jade');
 app.set('views','./views');
 
 app.use(express.static('./public'));
 
-var routes = require('./routes.js')(app);
+//Add express responseTime middleware
+app.use(responseTime());
 
-http.createServer(app).listen('3000',function() {
-	console.log('The server is listening 3000');
+app.use(express.Router());
+
+//app.use(logger('combined'));
+
+var routes = require('./routes.js')(app,config);
+
+app.use(errorHandler());
+
+http.createServer(app).listen(config.port,function() {
+	console.log('The server is listening ' + config.port );
 });
--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
-module.exports = function (app) {
+module.exports = function (app,config) {
+	//console.log(config);
 	app.get('/',function(req,res) {
-		//res.send('HelloWorld!');
-		res.render('index');
+		//console.log(config);
+		res.render('index', {title:config.title, message:config.message});
+		//fail();
 	});
 	app.get('/hello',function(req,res) {
 		//res.send('This is about page');
